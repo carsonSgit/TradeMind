96d348376f02e13eeda874cb3e14c2148d8c2f9b
kbaggott1 <kevin.baggott1@gmail.com>
2024-02-18 14:01:34 -0500

    Design doc added

--- web/frontend/trade-mind/src/pages/DesignDocument.js
+++ web/frontend/trade-mind/src/pages/DesignDocument.js
@@ -1,3 +1,31 @@
+import React, { useState, useEffect } from 'react';
+
 export default function DesignDocument() {
-    return <h1>Design Document</h1>
+  const [htmlContent, setHtmlContent] = useState('');
+
+  useEffect(() => {
+    const fetchHtmlContent = async () => {
+      try {
+        const cssLink = '<link rel="stylesheet" href="https://raw.githubusercontent.com/carsonSgit/TradeMind/main/design_doc/design_doc.css">';
+
+        const cssRes = await fetch('https://raw.githubusercontent.com/carsonSgit/TradeMind/main/design_doc/design_doc.css')
+        const css = await cssRes.text();
+        const response = await fetch('https://raw.githubusercontent.com/carsonSgit/TradeMind/main/design_doc/design_doc.html');
+        const html = await response.text();
+        // Prepend the CSS link to the HTML content
+        const htmlWithCss = html + "<style>" + css + "</style>";
+        setHtmlContent(htmlWithCss);
+      } catch (error) {
+        console.error("Failed to fetch HTML content:", error);
+        setHtmlContent('<p>Failed to load content.</p>');
+      }
+    };
+
+    fetchHtmlContent();
+  }, []); // Empty dependency array means this effect runs once after the initial render
+
+  return (
+    //<div dangerouslySetInnerHTML={{__html: htmlContent}} />
+    <iframe srcDoc={htmlContent} title='design' style={{width:'99%', height:'85vh', border: 'None'} }/>
+  );
 }
