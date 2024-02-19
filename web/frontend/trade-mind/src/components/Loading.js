d59a9968044c0dc1cb1232cd6622d07c2ec5ccf5
kbaggott1 <kevin.baggott1@gmail.com>
2024-02-19 14:20:12 -0500

    Turn loading into a component

---
+++ web/frontend/trade-mind/src/components/Loading.js
@@ -0,0 +1,12 @@
+export default function Loading({style}) {
+    return (
+        <div class="wrapper" style={style}>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="shadow"></div>
+        <div class="shadow"></div>
+        <div class="shadow"></div>
+    </div>
+    )
+}
