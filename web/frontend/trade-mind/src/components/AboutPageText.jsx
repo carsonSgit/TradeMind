1e4e0d746a28ab82582c37250eaf40c3e7d1075f
kbaggott1 <kevin.baggott1@gmail.com>
2024-02-18 20:39:23 -0500

    Add kevin to about page

--- web/frontend/trade-mind/src/components/AboutPageText.jsx
+++ web/frontend/trade-mind/src/components/AboutPageText.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const AboutPageText = ({ textContent, nameContent, side }) => {
+const AboutPageText = ({ textContent, nameContent, side, link }) => {
   return (
     <div className={`text ${side} summaryBlock`}>
-        <h2 className="devTitle">{nameContent}</h2>
+        <h2 className="devTitle"><a target="_blank" href={link}>{nameContent}</a></h2>
         <p className="devSummary">{textContent}</p>
     </div>
   );
--- web/frontend/trade-mind/src/pages/About.js
+++ web/frontend/trade-mind/src/pages/About.js
@@ -8,40 +8,50 @@ export default function About() {
     const cristianoSummary = "I'm a junior software developer that is dedicated to crafting efficient and user friendly solutions." + 
     "With expertise in numerous technologies thanks to both my education and work experience, I can provide solutions using a great range of technologies." + 
     "Let's work together to create something great!";
-    const kevinSummary = "I'm a junior software developer that is dedicated to crafting efficient and user friendly solutions." + 
-    "With expertise in numerous technologies thanks to both my education and work experience, I can provide solutions using a great range of technologies." + 
-    "Let's work together to create something great!";
+    const cristianoLinkedIn = "https://www.linkedin.com/in/cristiano-fazi-b21584208/"
+
+    const kevinSummary = "Hello! I'm Kevin, a passionate computer science enthusiast who enjoys programming and solving problems. " + 
+    "From a young age, I've always been fascinated by the world of computers. Because of that, I've dedicated my academic and" + 
+    " professional journey to this field of work. When I'm not coding, you can find me riding my motorcycle, going to the gym, snowboarding or " + 
+    "just hanging out with my friends and family.";
+    const kevinLinkedIn = "https://www.linkedin.com/in/kevin-baggott-2135a4241/"
+
     const carsonSummary = "I'm a junior software developer that is dedicated to crafting efficient and user friendly solutions." + 
     "With expertise in numerous technologies thanks to both my education and work experience, I can provide solutions using a great range of technologies." + 
     "Let's work together to create something great!";
+    const carsonLinkedIn = "https://www.linkedin.com/in/carson-spriggs-audet-609372217/"
+
     const cindySummary = "I'm a junior software developer that is dedicated to crafting efficient and user friendly solutions." + 
     "With expertise in numerous technologies thanks to both my education and work experience, I can provide solutions using a great range of technologies." + 
     "Let's work together to create something great!";
+    const cindyLinkedIn = "https://www.linkedin.com/in/cindy-coulibaly-93543b294/"
+
     const felixSummary = "I'm a junior game developer with a keen interest in AI, I bring a diverse skill set and a passion for innovation." + 
     "a junior game developer with a keen interest in AI, I bring a diverse skill set and a passion for innovation." + 
     "Let's work together to create something great!";
+    const felixLinkedIn = "https://www.linkedin.com/in/felix-st-laurent-816aa12b3/"
 
     return (
         <div className="container">
           <div className="row">
             <AboutPageImage imageUrl="logo512.png" side="left" />
-            <AboutPageText textContent={cristianoSummary} nameContent="Cristiano Fazi" side="right" />
+            <AboutPageText textContent={cristianoSummary} nameContent="Cristiano Fazi" side="right" link={cristianoLinkedIn} />
+          </div>
+          <div className="row">
+            <AboutPageImage imageUrl="about/kevin.webp" side="right" />
+            <AboutPageText textContent={kevinSummary} nameContent="Kevin Baggott" side="left" link={kevinLinkedIn} />
           </div>
           <div className="row">
             <AboutPageImage imageUrl="logo512.png" side="right" />
-            <AboutPageText textContent={kevinSummary} nameContent="Kevin Baggott" side="left" />
+            <AboutPageText textContent={carsonSummary} nameContent="Carson Spriggs" side="right" link={carsonLinkedIn}/>
           </div>
           <div className="row">
             <AboutPageImage imageUrl="logo512.png" side="right" />
-            <AboutPageText textContent={carsonSummary} nameContent="Carson Spriggs" side="right" />
+            <AboutPageText textContent={felixSummary} nameContent="Felix St-Laurent" side="left" link={felixLinkedIn}/>
           </div>
           <div className="row">
             <AboutPageImage imageUrl="logo512.png" side="right" />
-            <AboutPageText textContent={felixSummary} nameContent="Felix St-Laurent" side="left" />
-          </div>
-          <div className="row">
-            <AboutPageImage imageUrl="logo512.png" side="right" />
-            <AboutPageText textContent={cindySummary} nameContent="Cindy Coulibaly" side="right" />
+            <AboutPageText textContent={cindySummary} nameContent="Cindy Coulibaly" side="right" link={cindyLinkedIn}/>
           </div>
         </div>
       );
