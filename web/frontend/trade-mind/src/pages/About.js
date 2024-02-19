520944689b73e7379259d621f348d2161cebd51f
Carson <92652800+carsonSgit@users.noreply.github.com>
2024-02-19 10:53:29 -0500

    About page Carson Summary

--- web/frontend/trade-mind/src/pages/About.js
+++ web/frontend/trade-mind/src/pages/About.js
@@ -16,9 +16,9 @@ export default function About() {
     "just hanging out with my friends and family.";
     const kevinLinkedIn = "https://www.linkedin.com/in/kevin-baggott-2135a4241/"
 
-    const carsonSummary = "I'm a junior software developer that is dedicated to crafting efficient and user friendly solutions." + 
-    "With expertise in numerous technologies thanks to both my education and work experience, I can provide solutions using a great range of technologies." + 
-    "Let's work together to create something great!";
+    const carsonSummary = "Hi, my name is Carson! I'm currently a 3rd year computer science student with a focus on software development! " + 
+    "My first experience with coding was an extra-curricular program at the YMCA for a Web Programming course. Having not been a big fan of school, I was really surprised at how much I enjoyed taking that course. " + 
+    "Hobby-wise, I spend most of my free time either listening to music or working out.";
     const carsonLinkedIn = "https://www.linkedin.com/in/carson-spriggs-audet-609372217/"
 
     const cindySummary = "I'm a junior software developer that is dedicated to crafting efficient and user friendly solutions." + 
