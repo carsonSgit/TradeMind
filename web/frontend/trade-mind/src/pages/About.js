965b52879b95067e6b236c25b588b9841afe673f
Carson <92652800+carsonSgit@users.noreply.github.com>
2024-02-19 11:11:21 -0500

    About page Image UI Improvements

--- web/frontend/trade-mind/src/pages/About.js
+++ web/frontend/trade-mind/src/pages/About.js
@@ -21,8 +21,8 @@ export default function About() {
     "Hobby-wise, I spend most of my free time either listening to music or working out.";
     const carsonLinkedIn = "https://www.linkedin.com/in/carson-spriggs-audet-609372217/"
 
-    const cindySummary = "I'm a junior software developer that is dedicated to crafting efficient and user friendly solutions." + 
-    "With expertise in numerous technologies thanks to both my education and work experience, I can provide solutions using a great range of technologies." + 
+    const cindySummary = "I'm a junior software developer that is dedicated to crafting efficient and user friendly solutions. " + 
+    "With expertise in numerous technologies thanks to both my education and work experience, I can provide solutions using a great range of technologies. " + 
     "Let's work together to create something great!";
     const cindyLinkedIn = "https://www.linkedin.com/in/cindy-coulibaly-93543b294/"
 
