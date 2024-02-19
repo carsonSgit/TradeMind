9f25a65e24d37543cfa4ae6a60f64d7e412d505d
Cristiano Fazi <cristianofazi03@gmail.com>
2024-02-19 15:21:12 -0500

    Added Contact Info Icons

--- web/frontend/trade-mind/src/components/AboutPageText.jsx
+++ web/frontend/trade-mind/src/components/AboutPageText.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const AboutPageText = ({ textContent, nameContent, side, link }) => {
+const AboutPageText = ({ textContent, nameContent, side, site, github, linkdin, email}) => {
   return (
     <div className={`text ${side} summaryBlock`}>
         <button className="devTitle cta">
-        <span class="hover-underline-animation"><a target="blank" href={link}>{nameContent}</a></span> 
+        <span class="hover-underline-animation"><a target="blank" href={site}>{nameContent}</a></span> 
         <svg
           id="arrow-horizontal"
           xmlns="http://www.w3.org/2000/svg"
@@ -23,6 +23,60 @@ const AboutPageText = ({ textContent, nameContent, side, link }) => {
 
         </button>
         <p className="devSummary">{textContent}</p>
+        <div class="social-card">
+
+        <a target="blank" href={github}>
+          <button class="Btn github">
+            <span class="svgContainer">
+              <svg class="svgIcon" height="1.6em" viewBox="0 0 496 512" fill="#673AB7">
+                <path
+                  d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+                ></path>
+              </svg>
+            </span>
+            <span class="BG"></span>
+          </button>
+        </a>
+
+        <a href={"mailto:" + email}>
+          <button class="Btn whatsapp">
+            <span class="svgContainer">
+              <svg
+                class="svgIcon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.85em"
+                height="1.8em"
+                fill="#673AB7"
+                viewBox="100 100 100"
+              >
+                <path
+                  d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 2v2.42l-8 5.33-8-5.33V6h16zM4 6L12 9.58 20 6H4z"
+                ></path>
+              </svg>
+            </span>
+            <span class="BG"></span>
+          </button>
+        </a>
+
+        <a target='blank' href={linkdin}>
+          <button class="Btn linkdin">
+            <span class="svgContainer">
+              <svg
+                fill="#673AB7"
+                class="svgIcon"
+                xmlns="http://www.w3.org/2000/svg"
+                height="1.6em"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"
+                ></path>
+              </svg>
+            </span>
+            <span class="BG"></span>
+          </button>
+        </a>
+        </div>
     </div>
   );
 };
--- web/frontend/trade-mind/src/pages/About.js
+++ web/frontend/trade-mind/src/pages/About.js
@@ -10,6 +10,8 @@ export default function About() {
     "Let's work together to create something great!";
     const cristianoPortfolio = "https://cristiano-fazi.github.io/"
     const cristianoLinkedIn = "https://www.linkedin.com/in/cristiano-fazi-b21584208/"
+    const cristianoGithub = 'https://github.com/Cristiano-Fazi';
+    const cristianoEmail = "cristiano.fazi@bell.net";
 
     const kevinSummary = "Hello! I'm Kevin, a passionate computer science enthusiast who enjoys programming and solving problems. " + 
     "From a young age, I've always been fascinated by the world of computers. Because of that, I've dedicated my academic and" + 
@@ -17,46 +19,54 @@ export default function About() {
     "just hanging out with my friends and family.";
     const kevinPortfolio = "https://kbaggott1.github.io/";
     const kevinLinkedIn = "https://www.linkedin.com/in/kevin-baggott-2135a4241/"
+    const kevinGithub = "https://github.com/kbaggott1";
+    const kevinEmail = "kevin.baggott1@gmail.com";
 
     const carsonSummary = "Hi, my name is Carson! I'm currently a 3rd year computer science student with a focus on software development! " + 
     "My first experience with coding was an extra-curricular program at the YMCA for a Web Programming course. Having not been a big fan of school, I was really surprised at how much I enjoyed taking that course. " + 
     "Hobby-wise, I spend most of my free time either listening to music or working out.";
     const carsonPortfolio = "https://carsonsgit.github.io/";
     const carsonLinkedIn = "https://www.linkedin.com/in/carson-spriggs-audet-609372217/"
+    const carsonGithub = "https://github.com/carsonSgit";
+    const carsonEmail = "carsonspriggs8@gmail.com";
 
     const cindySummary = "Hi there, I'm Cindy, currently a third-year Computer Science student at John Abbott College. " + 
     "My passion for computers and technology has been with me since childhood. In my free time, " + 
     "you can often find me working out, diving into a good book, or enjoying some music.";
     const cindyPortfolio = "https://cindy-coulibaly.github.io/";
     const cindyLinkedIn = "https://www.linkedin.com/in/cindy-coulibaly-93543b294/"
+    const cindyGithub = "https://github.com/Cindy-Coulibaly";
+    const cindyEmail = "";
 
     const felixSummary = "I'm a junior game developer with a keen interest in AI, I bring a diverse skill set and a passion for innovation." + 
     "a junior game developer with a keen interest in AI, I bring a diverse skill set and a passion for innovation." + 
     "Let's work together to create something great!";
     const felixPortfolio = "https://github.com/Felixs123";
     const felixLinkedIn = "https://www.linkedin.com/in/felix-st-laurent-816aa12b3/"
+    const felixGithub = "https://github.com/Felixs123";
+    const felixEmail = "felix.stlaurent123@gmail.com";
 
     return (
         <div className="container">
           <div className="row">
             <AboutPageImage imageUrl="../about/kevin.webp" side="left" />
-            <AboutPageText textContent={kevinSummary} nameContent="Kevin Baggott" side="right" link={kevinPortfolio} />
+            <AboutPageText textContent={kevinSummary} nameContent="Kevin Baggott" side="right" site={kevinPortfolio} github={kevinGithub} linkdin={kevinLinkedIn} email={cristianoEmail}/>
           </div>
           <div className="row">
             <AboutPageImage imageUrl="../about/cristianoImage.jpg" side="right" />
-            <AboutPageText textContent={cristianoSummary} nameContent="Cristiano Fazi" side="left" link={cristianoPortfolio} />
+            <AboutPageText textContent={cristianoSummary} nameContent="Cristiano Fazi" side="left" site={cristianoPortfolio} github={cristianoGithub} linkdin={cristianoLinkedIn} email={kevinEmail}/>
           </div>
           <div className="row">
             <AboutPageImage imageUrl="../about/carsonspriggs.jpg" side="right" />
-            <AboutPageText textContent={carsonSummary} nameContent="Carson Spriggs" side="right" link={carsonPortfolio}/>
+            <AboutPageText textContent={carsonSummary} nameContent="Carson Spriggs" side="right" site={carsonPortfolio} github={carsonGithub} linkdin={carsonLinkedIn} email={carsonEmail}/>
           </div>
           <div className="row">
             <AboutPageImage imageUrl="logo512.png" side="right" />
-            <AboutPageText textContent={felixSummary} nameContent="Felix St-Laurent" side="left" link={felixPortfolio}/>
+            <AboutPageText textContent={felixSummary} nameContent="Felix St-Laurent" side="left" site={felixPortfolio} github={felixGithub} linkdin={felixLinkedIn} email={felixEmail}/>
           </div>
           <div className="row">
             <AboutPageImage imageUrl="../about/cindyIcon.jpg" side="right" />
-            <AboutPageText textContent={cindySummary} nameContent="Cindy Coulibaly" side="right" link={cindyPortfolio}/>
+            <AboutPageText textContent={cindySummary} nameContent="Cindy Coulibaly" side="right" site={cindyPortfolio} github={cindyGithub} linkdin={cindyLinkedIn} email={cindyEmail}/>
           </div>
         </div>
       );
