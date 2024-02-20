0755fb2ecb89b3a509e3ac5ddefb7825872656ad
kbaggott1 <kevin.baggott1@gmail.com>
2024-02-19 20:37:07 -0500

    Home page

--- web/frontend/trade-mind/src/pages/Home.js
+++ web/frontend/trade-mind/src/pages/Home.js
@@ -1,3 +1,22 @@
+import { Link } from 'react-router-dom'
+import './Home.css'
+import { useNavigate } from 'react-router-dom'
 export default function Home() {
-    return <h1>Home</h1>
+    let navigate = useNavigate();
+
+    return (
+        <div className={"landingContainer"}>
+            <link href="https://fonts.cdnfonts.com/css/norwester" rel="stylesheet" />         
+
+            <img src="../hero.jpg"></img>
+            <div className='titleContainer'>
+                <h1>TRADEMIND</h1>
+                <div className='buttonContainer'>
+                    <button>Our Mission</button>
+                    <button onClick={() => navigate('about')}>Our Team</button>
+                    <button>Our Product</button>
+                </div>
+            </div>
+        </div>
+    )
 }
