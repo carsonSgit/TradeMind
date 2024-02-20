986339448e51254ae3cd93f1f45e6e5c2c8d035a
kbaggott1 <kevin.baggott1@gmail.com>
2024-02-19 22:29:39 -0500

    Add divider to home

--- web/frontend/trade-mind/src/pages/Home.js
+++ web/frontend/trade-mind/src/pages/Home.js
@@ -5,18 +5,25 @@ export default function Home() {
     let navigate = useNavigate();
 
     return (
-        <div className={"landingContainer"}>
-            <link href="https://fonts.cdnfonts.com/css/norwester" rel="stylesheet" />         
+        <>
+            <div className={"landingContainer wave-container"}>
+                <link href="https://fonts.cdnfonts.com/css/norwester" rel="stylesheet" />         
 
-            <img src="../hero.jpg"></img>
-            <div className='titleContainer'>
-                <h1>TRADEMIND</h1>
-                <div className='buttonContainer'>
-                    <button>Our Mission</button>
-                    <button onClick={() => navigate('about')}>Our Team</button>
-                    <button>Our Product</button>
+                <img src="../hero.jpg"></img>
+                <div className='titleContainer'>
+                    <h1>TRADEMIND</h1>
+                    <div className='buttonContainer'>
+                        <button>Our Mission</button>
+                        <button onClick={() => navigate('about')}>Our Team</button>
+                        <button>Our Product</button>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className={'contentContainer'} >
+                <h1>Content will go under heresies</h1>
+            </div>
+        </>
+
+
     )
 }
