ee1d112a3bd559c4e949c54d6335f40b39308d73
kbaggott1 <kevin.baggott1@gmail.com>
2024-02-20 19:04:56 -0500

    Add plotly

--- web/frontend/trade-mind/src/pages/Analytics.js
+++ web/frontend/trade-mind/src/pages/Analytics.js
@@ -1,3 +1,10 @@
+import DataPlot from "../components/DataPlot";
+
 export default function Analytics() {
-    return <h1>Analytics</h1>
+    return (
+        <>
+            <h1>Analytics</h1>
+            <DataPlot />
+        </>
+    )
 }
