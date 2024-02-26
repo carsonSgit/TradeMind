0c2203501dc4afff45c6ee433e33cc215def5c68
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-02-25 21:51:15 -0500

    Analytics navbar fix

--- web/frontend/trade-mind/src/components/DataPlot.jsx
+++ web/frontend/trade-mind/src/components/DataPlot.jsx
@@ -4,19 +4,26 @@ import Plot from 'react-plotly.js';
 class DataPlot extends React.Component {
     render() {
         return (
-            <Plot
-                data={[
-                    {
-                        x: [1, 2, 3],
-                        y: [2, 6, 3],
-                        type: 'scatter',
-                        mode: 'lines+markers',
-                        marker: {color: 'red'},
-                    },
-                    {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-                ]}
-                layout={{width: 1000, height: 800, title: 'A Fancy Plot'}}
-            />
+            <div style={{width: '100%', height: '100%'}}>
+                <Plot
+                    data={[
+                        {
+                            x: [1, 2, 3],
+                            y: [2, 6, 3],
+                            type: 'scatter',
+                            mode: 'lines+markers',
+                            marker: {color: 'red'},
+                        },
+                        {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+                    ]}
+                    layout={
+                        {
+                            autosize: true,
+                            title: 'A Fancy Plot'
+                        }
+                    }
+                />
+            </div>
         );
     }
 }
