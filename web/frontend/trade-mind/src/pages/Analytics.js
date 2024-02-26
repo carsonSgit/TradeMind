b857340b00c6801380cb1944bbdede7e8baaa3a1
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-02-25 19:43:56 -0500

    Updated temp text

--- web/frontend/trade-mind/src/pages/Analytics.js
+++ web/frontend/trade-mind/src/pages/Analytics.js
@@ -6,8 +6,15 @@ export default function Analytics() {
         <>
             <div className='analytics-title'> Analytics </div>
             <div className='text'>
-                <p>We're currently working on enhancing this page to provide valuable analytics insights.</p>
-                <p>Stay tuned for updates!</p>
+                <p>
+                    We are working on our AI model at the moment, but soon you will be able to see stock analytics for every stock
+                    in the S&P 500. You will be able to access the full potential of our TradeMind AI to better your knowledge & allow you
+                    to make more informed decisions!
+                </p>
+                <p>
+                    Stocks are a great way to invest your money, but it can be hard to know which stocks to invest in and when to make that
+                    investment! We are here to help you with that.
+                </p>
             </div>
             <DataPlot />
         </>
