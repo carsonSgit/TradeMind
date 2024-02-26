bf4e198103f432f99be963e7ddf9ae3cf739aae1
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-02-25 22:02:25 -0500

    Added documentation so it's clearer

--- web/frontend/trade-mind/src/components/DataPlot.jsx
+++ web/frontend/trade-mind/src/components/DataPlot.jsx
@@ -4,31 +4,33 @@ import Plot from 'react-plotly.js';
 class DataPlot extends React.Component {
     render() {
         return (
-            <div style={{width: '70%', height: '100%', overflow: 'hidden'}}>
-                <Plot
-                    data={[
-                        {
-                            x: [1, 2, 3],
-                            y: [2, 6, 3],
-                            type: 'scatter',
-                            mode: 'lines+markers',
-                            marker: {color: 'red'},
-                        },
-                        {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-                    ]}
-                    layout={
-                        {
-                            autosize: true,
-                            margin: { t: 50, b: 50, l: 50, r: 50 },
-                            title: 'A Fancy Plot'
-                        }
-                    }               
-                    style={{ width: '100%', height: '100%' }}
-                    useResizeHandler={true}
-                />
-            </div>
-        );
+                // Styling for the container div
+                <div style={{width: '70%', height: '100%', overflow: 'hidden'}}>
+                    <Plot
+                        // Data to be plotted
+                        data={[
+                            {
+                                x: [1, 2, 3],
+                                y: [2, 6, 3],
+                                type: 'scatter',
+                                mode: 'lines+markers',
+                                marker: {color: 'red'},
+                            },
+                            {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},  // Chart data
+                        ]}
+                        layout={{
+                            autosize: true,  // Automatically adjust size
+                            margin: { t: 50, b: 50, l: 50, r: 50 },  // Margins
+                            title: 'A Fancy Plot'  // Plot title
+                        }}
+                        // Styling for the plot component
+                        style={{ width: '100%', height: '100%' }}
+                        // Enable resizing of the plot
+                        useResizeHandler={true}
+                    />
+                </div>
+            );
+        }
     }
-}
 
 export default DataPlot;
