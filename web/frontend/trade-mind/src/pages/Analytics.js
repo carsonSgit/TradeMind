a82785c6e0965023880ec0399de2f7c636dcb4cf
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-02-25 20:34:58 -0500

    UI fix on Analytics

--- web/frontend/trade-mind/src/pages/Analytics.js
+++ web/frontend/trade-mind/src/pages/Analytics.js
@@ -5,7 +5,7 @@ export default function Analytics() {
     return (
         <>
             <div className='analytics-title'> Analytics </div>
-            <div className='text'>
+            <div className='analytics-text'>
                 <p>
                     We are working on our AI model at the moment, but soon you will be able to see stock analytics for every stock
                     in the S&P 500. You will be able to access the full potential of our TradeMind AI to better your knowledge & allow you
