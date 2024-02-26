3cd875edf5939ed253392dc6c392be683f34188a
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-02-25 21:55:31 -0500

    Fix overflow issue
    Was causing the navbar to break but page doesn't have any issue

--- web/frontend/trade-mind/src/components/DataPlot.jsx
+++ web/frontend/trade-mind/src/components/DataPlot.jsx
@@ -4,7 +4,7 @@ import Plot from 'react-plotly.js';
 class DataPlot extends React.Component {
     render() {
         return (
-            <div style={{width: '100%', height: '100%'}}>
+            <div style={{width: '70%', height: '100%', overflow: 'hidden'}}>
                 <Plot
                     data={[
                         {
